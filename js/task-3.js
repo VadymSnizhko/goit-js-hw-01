0721764e90344cea329a93c4577988719ed76ec6
vadim <snizhko@gmail.com>
2025-01-24 22:51:06 +0200

    finish 3 task

---
+++ js/task-3.js
@@ -0,0 +1,8 @@
+function getElementWidth(content, padding, border) {
+
+    let totalWidth = parseFloat(content) + (parseFloat(padding) * 2) + (parseFloat(border) * 2);
+
+    let message = totalWidth;
+    
+    return message;
+}
